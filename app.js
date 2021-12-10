0f2d230f7bdc7a919e6bb6d94977b5066fd8a0f4
LocalBoy <jose.ariasma@ug.edu.ec>
2021-12-09 23:09:13 -0500

    se agrega la practica

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    let mensaje = "Hola";
+    let numero = 123;
+    let bloeano = true;
+    let hoy = new Date();
+    let cosa;
+    cosa = mensaje;
+    cosa = numero;
+    cosa = hoy;
+    let spiderman = {
+        nombre: "el pepe",
+        edad: 30,
+        poder: "comedia"
+    };
+    spiderman = { nombre: "etesetch", edad: 40, poder: "eteseeetch" };
+})();
---
+++ app.ts
@@ -0,0 +1,36 @@
+(function() { 
+
+
+let mensaje:string = "Hola";
+
+let numero:number = 123;
+
+let bloeano : boolean = true;
+
+let hoy  = new Date ();
+
+
+
+
+let cosa;
+cosa = mensaje;
+cosa = numero;
+cosa = hoy;
+
+
+let spiderman = {
+
+    nombre: "el pepe",
+    edad : 30,
+    poder: "comedia"
+};
+
+spiderman = {nombre:"etesetch",edad:40,poder:"eteseeetch"};
+
+
+
+
+
+
+
+})();
